2b45677376b9cdcc7e989ed80509d8feb72e4599
nanqiang <nanqianglin@gmail.com>
2022-07-20 14:13:50 +0800

    fix: the image url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,14 @@ import * as React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import createEmotionServer from '@emotion/server/create-instance'
 import createEmotionCache from '../src/createEmotionCache'
+import { prefix } from '../utils/prefix'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href={`${prefix}/favicon.ico`} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
