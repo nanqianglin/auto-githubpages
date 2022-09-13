7ef43295eaad2b36868766bf7ee5e9e184aa6b3e
nanqianglin <107928296+nanqianglin@users.noreply.github.com>
2022-09-13 09:15:50 +0000

    Deploying to gh-pages from @ nanqianglin/auto-githubpages@7f4804b79aa4b0bf4f472bdbabbc96fba44e0500 🚀

---
+++ _next/static/L0StfTw6LIC1Ud2RgUAi5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/250-0af1fcfec0cd6df8.js","static/chunks/pages/index-8e1b0818ff2e7591.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
